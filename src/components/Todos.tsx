951a619edfb22610107cba16560de0a184e2dfcb
PhuCT <123@123>
2021-09-15 14:37:22 +0700

    first commit

---
+++ src/components/Todos.tsx
@@ -0,0 +1,26 @@
+import React, { useContext } from "react";
+
+import Todo from "../models/todo";
+import { TodosContext } from "../store/todos-context";
+import TodoItem from "./TodoItem";
+import classes from "./Todos.module.css";
+
+const Todos: React.FC = () => {
+  const todosCtx = useContext(TodosContext);
+
+  return (
+    <ul className={classes.todos}>
+      {todosCtx.items.map((item) => {
+        return (
+          <TodoItem
+            text={item.text}
+            key={item.id}
+            onRemoveTodo={todosCtx.removeTodo.bind(null, item.id)}
+          />
+        );
+      })}
+    </ul>
+  );
+};
+
+export default Todos;
